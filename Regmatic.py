c23953cb3a932b15897d40c53c4fa6414a34e8b1
Steve Pieper <pieper@isomics.com>
2012-06-15 17:31:20 -0400

    ENH: set up logic optimizer framework

--- Regmatic.py
+++ Regmatic.py
@@ -35,6 +35,8 @@ class RegmaticWidget:
       self.setup()
       self.parent.show()
 
+    self.logic = RegmaticLogic()
+
   def setup(self):
     # Instantiate and connect widgets ...
 
@@ -54,48 +56,77 @@ class RegmaticWidget:
     parameterFormLayout = qt.QFormLayout(parameterCollapsibleButton)
 
     # Fixed Volume node selector
-    fixedSelector = slicer.qMRMLNodeComboBox()
-    fixedSelector.objectName = 'fixedSelector'
-    fixedSelector.toolTip = "The fixed volume."
-    fixedSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
-    fixedSelector.noneEnabled = False
-    fixedSelector.addEnabled = False
-    fixedSelector.removeEnabled = False
-    parameterFormLayout.addRow("Fixed Volume:", fixedSelector)
-    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)', 
-                        fixedSelector, 'setMRMLScene(vtkMRMLScene*)')
+    self.fixedSelector = slicer.qMRMLNodeComboBox()
+    self.fixedSelector.objectName = 'fixedSelector'
+    self.fixedSelector.toolTip = "The fixed volume."
+    self.fixedSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
+    self.fixedSelector.noneEnabled = False
+    self.fixedSelector.addEnabled = False
+    self.fixedSelector.removeEnabled = False
+    parameterFormLayout.addRow("Fixed Volume:", self.fixedSelector)
+    self.fixedSelector.setMRMLScene(slicer.mrmlScene)
+    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)',
+                        self.fixedSelector, 'setMRMLScene(vtkMRMLScene*)')
 
     # Moving Volume node selector
-    movingSelector = slicer.qMRMLNodeComboBox()
-    movingSelector.objectName = 'movingSelector'
-    movingSelector.toolTip = "The moving volume."
-    movingSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
-    movingSelector.noneEnabled = False
-    movingSelector.addEnabled = False
-    movingSelector.removeEnabled = False
-    parameterFormLayout.addRow("Moving Volume:", movingSelector)
-    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)', 
-                        movingSelector, 'setMRMLScene(vtkMRMLScene*)')
+    self.movingSelector = slicer.qMRMLNodeComboBox()
+    self.movingSelector.objectName = 'movingSelector'
+    self.movingSelector.toolTip = "The moving volume."
+    self.movingSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
+    self.movingSelector.noneEnabled = False
+    self.movingSelector.addEnabled = False
+    self.movingSelector.removeEnabled = False
+    parameterFormLayout.addRow("Moving Volume:", self.movingSelector)
+    self.movingSelector.setMRMLScene(slicer.mrmlScene)
+    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)',
+                        self.movingSelector, 'setMRMLScene(vtkMRMLScene*)')
 
     # Transform node selector
-    transformSelector = slicer.qMRMLNodeComboBox()
-    transformSelector.objectName = 'transformSelector'
-    transformSelector.toolTip = "The transform volume."
-    transformSelector.nodeTypes = ['vtkMRMLScalarVolumeNode']
-    transformSelector.noneEnabled = False
-    transformSelector.addEnabled = False
-    transformSelector.removeEnabled = False
-    parameterFormLayout.addRow("Moving To Fixed Transform:", transformSelector)
-    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)', 
-                        transformSelector, 'setMRMLScene(vtkMRMLScene*)')
+    self.transformSelector = slicer.qMRMLNodeComboBox()
+    self.transformSelector.objectName = 'transformSelector'
+    self.transformSelector.toolTip = "The transform volume."
+    self.transformSelector.nodeTypes = ['vtkMRMLLinearTransformNode']
+    self.transformSelector.noneEnabled = False
+    self.transformSelector.addEnabled = False
+    self.transformSelector.removeEnabled = False
+    parameterFormLayout.addRow("Moving To Fixed Transform:", self.transformSelector)
+    self.transformSelector.setMRMLScene(slicer.mrmlScene)
+    self.parent.connect('mrmlSceneChanged(vtkMRMLScene*)',
+                        self.transformSelector, 'setMRMLScene(vtkMRMLScene*)')
+    selectors = (self.fixedSelector, self.movingSelector, self.transformSelector)
+    for selector in selectors:
+      selector.connect('currentNodeChanged(vtkMRMLNode*)', self.updateLogicFromGUI)
+
+    # Run button
+    self.runButton = qt.QPushButton("Run")
+    self.runButton.toolTip = "Run registration bot."
+    self.runButton.checkable = True
+    parameterFormLayout.addRow(self.runButton)
+    self.runButton.connect('toggled(bool)', self.onRunButtonToggled)
 
     # to support quicker development:
     import os
     if os.environ['USER'] == 'pieper':
-      RegmaticLogic().testingData()
+      self.logic.testingData()
+      self.fixedSelector.setCurrentNode(slicer.util.getNode('MRHead*'))
+      self.movingSelector.setCurrentNode(slicer.util.getNode('neutral*'))
+      self.transformSelector.setCurrentNode(slicer.util.getNode('movingToFixed*'))
 
     # Add vertical spacer
     self.layout.addStretch(1)
+
+  def updateLogicFromGUI(self):
+    self.logic.fixed = self.fixedSelector.currentNode()
+    self.logic.moving = self.movingSelector.currentNode()
+    self.logic.transform = self.transformSelector.currentNode()
+
+  def onRunButtonToggled(self, checked):
+    if checked:
+      self.logic.start()
+      self.runButton.text = "Stop"
+    else:
+      self.logic.stop()
+      self.runButton.text = "Run"
 
   def onReload(self,moduleName="Regmatic"):
     """Generic reload method for any scripted module.
@@ -104,7 +135,7 @@ class RegmaticWidget:
     import imp, sys, os, slicer
 
     widgetName = moduleName + "Widget"
-    
+
     # reload the source code
     # - set source file path
     # - load the module to the global space
@@ -135,15 +166,45 @@ class RegmaticWidget:
 #
 
 class RegmaticLogic(object):
-  """ Implement a template matching optimizer that is 
+  """ Implement a template matching optimizer that is
   integrated with the slicer main loop.
   Note: currently depends on numpy/scipy installation in mac system
   """
 
-  def __init__(self):
+  def __init__(self,fixed=None,moving=None,transform=None):
+    self.interval = 20
+    self.timer = None
+
+    # slicer nodes set by the GUI
+    self.fixed = fixed
+    self.moving = moving
+    self.transform = transform
+
+  def start(self):
+    """Create the subprocess and set up a polling timer"""
+    if self.timer:
+      self.stop()
+    self.timer = qt.QTimer()
+    self.timer.setInterval(self.interval)
+    self.timer.connect('timeout()', self.tick)
+    self.timer.start()
+
+  def stop(self):
+    if self.timer:
+      self.timer.stop()
+      self.timer = None
+
+  def tick(self):
+    """Callback for an iteration of the registration method
+    """
     pass
 
+
+
   def testingData(self):
+    """Load some default data for development
+    and set up a transform and viewing scenario for it.
+    """
     if not slicer.util.getNodes('MRHead*'):
       import os
       fileName = os.environ['HOME'] + "/Dropbox/data/regmatic/MR-head.nrrd"
@@ -154,14 +215,14 @@ class RegmaticLogic(object):
       fileName = os.environ['HOME'] + "/Dropbox/data/regmatic/neutral.nrrd"
       vl = slicer.modules.volumes.logic()
       volumeNode = vl.AddArchetypeScalarVolume (fileName, "neutral", 0)
-    if not slicer.util.getNodes('Transform-head'):
+    if not slicer.util.getNodes('movingToFixed'):
       # Create transform node
       transform = slicer.vtkMRMLLinearTransformNode()
-      transform.SetName('Transform-head')
+      transform.SetName('movingToFixed')
       slicer.mrmlScene.AddNode(transform)
     head = slicer.util.getNode('MRHead')
     neutral = slicer.util.getNode('neutral')
-    transform = slicer.util.getNode('Transform-head')
+    transform = slicer.util.getNode('movingToFixed')
     neutral.SetAndObserveTransformNodeID(transform.GetID())
     compositeNodes = slicer.util.getNodes('vtkMRMLSliceCompositeNode*')
     for compositeNode in compositeNodes.values():
@@ -170,3 +231,58 @@ class RegmaticLogic(object):
       compositeNode.SetForegroundOpacity(0.5)
     applicationLogic = slicer.app.applicationLogic()
     applicationLogic.FitSliceToAll()
+
+
+  """rotation/quaternion utilities
+    See:  http://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation#Conversion_to_and_from_the_matrix_representation
+  """
+
+  def rotation(self,quat):
+    """ build and return a 3x3 transformation matrix from a quaternion
+        >>> rotation([1,0,0,0])
+        array([[ 1.,  0.,  0.],
+               [ 0.,  1.,  0.],
+               [ 0.,  0.,  1.]])
+    """
+
+    quat = numpy.asmatrix(quat, dtype='Float64')
+    quat /= numpy.sqrt(quat * quat.transpose())
+    a,b,c,d           =  numpy.asarray(quat)[0]
+    aa, bb, cc, dd    =  a*a, b*b, c*c, d*d
+    ab, ac, ad        =  a*b, a*c, a*d
+    bc, bd, cd        =  b*c, b*d, c*d
+
+    Rot = numpy.matrix([[ aa + bb - cc - dd,  2.0 * (-ad + bc),    2.0*(ac + bd)     ],
+                      [ 2.0 * (ad + bc),   aa - bb + cc - dd,  2.0 * (-ab + cd)   ],
+                      [ 2.0 * (ac + bd),    2.0 * (ab + cd),   aa - bb - cc + dd ]])
+    return(Rot)
+
+
+  def quaternion(self,Rot):
+    """ extract the quaternion from a matrix (assume upper 3x3 is rotation only)
+    """
+
+    # find the permutation that is most stable (largest diagonal first)
+    xx, yy, zz = abs(Rot[0,0]), abs(Rot[1,1]), abs(Rot[2,2])
+    if xx > yy and xx > zz:
+      u, v, w = 0, 1, 2
+    else:
+      if yy > xx and yy > zz:
+        u, v, w = 1, 2, 0
+      else:
+        u, v, w = 2, 0, 1
+
+    # fill in the quat
+    r = numpy.sqrt( 1 + Rot[u,u] - Rot[v,v] - Rot[w,w] )
+    r2 = 2.*r
+
+    if r < 1e-6:
+      return numpy.array([1,0,0,0])
+
+    quat = numpy.zeros(4)
+    quat[  0] = (Rot[w,v] - Rot[v,w])/r2
+    quat[1+u] = r/2.
+    quat[1+v] = (Rot[u,v] + Rot[v,u])/r2
+    quat[1+w] = (Rot[w,u] + Rot[u,w])/r2
+
+    return quat
